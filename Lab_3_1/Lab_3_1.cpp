dc245b9cc62b39e795fa489f62814d5e0e606d35
Tylko-jedno <diogkek@gmail.com>
2024-09-14 19:05:23 +0300

    first master commit

---
+++ Lab_3_1/Lab_3_1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double x; //вхідні данні
+    double y; // відповідь
+    double a; // проміжний результат - функціонально стала частина виразу
+    double b; // проміжний результат - функціонально змінна частина виразу
+
+    cout << "x = "; cin >> x;
+
+    a = 1 / x + 4;
+
+    if (x < 1)
+        b = 0, 65 * x + 8;
+    if (x >= 1 && x < 5)
+        b = atan((x + 6, 1) / 2 + exp(x));
+    if (x > 5)
+        b = sqrt(1 + sqrt(x));
+
+    y = a - b;
+
+    cout << "1)y = " << y << endl;
+
+    if (x < 1)
+        b = 0, 65 * x + 8;
+    else {
+        if (x < 5)
+            b = atan((x + 6, 1) / 2 + exp(x));
+        else
+            b = sqrt(1 + sqrt(x));
+    }
+
+    y = a - b;
+
+    cout << "2)y = " << y << endl;
+}
